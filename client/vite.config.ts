88adbf9943e59fd917c5be5f2b5789f04c690b51
profangrybeard <prof.angrybeard@gmail.com>
2025-12-03 17:19:15 -0500

    made a build and added docs directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Malifaux4E_GAME356/", // repo name
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
+});
